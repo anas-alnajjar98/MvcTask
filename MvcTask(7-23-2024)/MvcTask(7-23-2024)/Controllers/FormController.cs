8b4d0342dd80c1a68854c83b532ce1b168ba4ce1
anas-alnajjar98 <160442902+anas-alnajjar98@users.noreply.github.com>
2024-07-24 10:24:35 +0300

    new task

---
+++ MvcTask(7-23-2024)/MvcTask(7-23-2024)/Controllers/FormController.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace MvcTask_7_23_2024_.Controllers
+{
+    public class FormController : Controller
+    {
+        // GET: Form
+        public ActionResult FormRegs()
+        {
+            return View();
+        }
+        [HttpPost]
+        public ActionResult FormContent(FormCollection form) {
+            ViewBag.name = form["name"];
+            ViewBag.num = form["num"];
+            ViewBag.gender = form["gender"];
+            ViewBag.Cont = form["Cont"];
+            ViewBag.inter = form["inter"];
+            return View();
+        }
+    }
+}
---
+++ MvcTask(7-23-2024)/MvcTask(7-23-2024)/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="MvcTask_7_23_2024_.MvcApplication" Language="C#" %>
