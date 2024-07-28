0649dad8f9b879bef2180e787a9dc5d922dfe60e
anas-alnajjar98 <160442902+anas-alnajjar98@users.noreply.github.com>
2024-07-29 00:01:27 +0300

    new task

---
+++ MvcTASK(7-28-2024)/MvcTASK(7-28-2024)/Controllers/IPHONEController.cs
@@ -0,0 +1,137 @@
+﻿using System.IO;
+using System.Linq;
+using System.Net;
+using System.Web;
+using System.Web.Mvc;
+using MvcTASK_7_28_2024_.Models;
+
+namespace MvcTASK_7_28_2024_.Controllers
+{
+    public class IPHONEController : Controller
+    {
+        private IPHONEEntities DB = new IPHONEEntities();
+
+        // GET: IPHONE
+        public ActionResult Index()
+        {
+            return View(DB.IPHONEINFOes.ToList());
+        }
+
+        // GET: IPHONE/Details/5
+        public ActionResult Details(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            IPHONEINFO iphone = DB.IPHONEINFOes.Find(id);
+            if (iphone == null)
+            {
+                return HttpNotFound();
+            }
+            return View(iphone);
+        }
+
+        // GET: IPHONE/Create
+        public ActionResult Create()
+        {
+            return View();
+        }
+
+        // POST: IPHONE/Create
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Create([Bind(Include = "NAME,PRICE,IMAGE")] IPHONEINFO iphone, HttpPostedFileBase ImageFile)
+        {
+            if (ModelState.IsValid)
+            {
+                if (ImageFile != null && ImageFile.ContentLength > 0)
+                {
+                    string fileName = Path.GetFileName(ImageFile.FileName);
+                    string path = Path.Combine(Server.MapPath("~/img"), fileName);
+                    ImageFile.SaveAs(path);
+                    iphone.IMAGE = "~/img/" + fileName;
+                }
+
+                DB.IPHONEINFOes.Add(iphone);
+                DB.SaveChanges();
+                return RedirectToAction("Index");
+            }
+
+            return View(iphone);
+        }
+
+        // GET: IPHONE/Edit/5
+        [HttpGet]
+        public ActionResult Edit(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            IPHONEINFO iphone = DB.IPHONEINFOes.Find(id);
+            if (iphone == null)
+            {
+                return HttpNotFound();
+            }
+            return View(iphone);
+        }
+
+        // POST: IPHONE/Edit/5
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Edit([Bind(Include = "ID,NAME,PRICE,IMAGE")] IPHONEINFO iphone, HttpPostedFileBase ImageFile)
+        {
+            if (ModelState.IsValid)
+            {
+                if (ImageFile != null && ImageFile.ContentLength > 0)
+                {
+                    string fileName = Path.GetFileName(ImageFile.FileName);
+                    string path = Path.Combine(Server.MapPath("~/img"), fileName);
+                    ImageFile.SaveAs(path);
+                    iphone.IMAGE = "~/img/" + fileName;
+                }
+
+                DB.Entry(iphone).State = System.Data.Entity.EntityState.Modified;
+                DB.SaveChanges();
+                return RedirectToAction("Index");
+            }
+            return View(iphone);
+        }
+
+        // GET: IPHONE/Delete/5
+        public ActionResult Delete(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            IPHONEINFO iphone = DB.IPHONEINFOes.Find(id);
+            if (iphone == null)
+            {
+                return HttpNotFound();
+            }
+            return View(iphone);
+        }
+
+        // POST: IPHONE/Delete/5
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public ActionResult DeleteConfirmed(int id)
+        {
+            IPHONEINFO iphone = DB.IPHONEINFOes.Find(id);
+            DB.IPHONEINFOes.Remove(iphone);
+            DB.SaveChanges();
+            return RedirectToAction("Index");
+        }
+
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing)
+            {
+                DB.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+    }
+}
---
+++ MvcTASK(7-28-2024)/MvcTASK(7-28-2024)/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="MvcTASK_7_28_2024_.MvcApplication" Language="C#" %>
