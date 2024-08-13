09fd121bf32563fdd0cf62cd996ca5bd3ddafa4d
anas-alnajjar98 <160442902+anas-alnajjar98@users.noreply.github.com>
2024-08-14 01:57:08 +0300

    new task

---
+++ taskfortoday/taskfortoday/Controllers/form2Controller.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace taskfortoday.Controllers
+{
+    public class form2Controller : Controller
+    {
+        // GET: form2
+        public ActionResult Index()
+        {
+            List<Dictionary<string, string>> products = Session["Products"] as List<Dictionary<string, string>>;
+
+            if (products == null)
+            {
+                products = (List<Dictionary<string, string>>)Session["Products"];
+            }
+
+            return View(products);
+        }
+
+        [HttpPost]
+
+        public ActionResult CreateForm(string productName, string productPrice, string productImage) {
+            List<Dictionary<string, string>> products = Session["Products"] as List<Dictionary<string, string>>;
+
+            if (products == null)
+            {
+                products = new List<Dictionary<string, string>>();
+            }
+
+
+            Dictionary<string, string> product = new Dictionary<string, string>
+            {
+                {"Name",productName},
+                {"Price", productPrice},
+                {"Image",productImage}
+            };
+
+            products.Add(product);
+            Session["Products"] = products;
+
+            return View();
+
+        }
+    }
+}
---
+++ taskfortoday/taskfortoday/Controllers/formController.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Web.Mvc;
+
+namespace taskfortoday.Controllers
+{
+    public class Item
+    {
+        public string Name { get; set; }
+        public string Price { get; set; }
+        public string Image { get; set; }
+    }
+
+    public class FormController : Controller
+    {
+        // GET: Form
+        public ActionResult Index()
+        { 
+            var items = Session["ProductInfo"] as List<Item> ?? new List<Item>();
+            return View(items);
+        }
+
+        public ActionResult CreateForm() {
+        return View();
+        }
+        [HttpPost]
+        public ActionResult CreateForm(string name, string price, string image)
+        {
+            
+            var items = Session["ProductInfo"] as List<Item> ?? new List<Item>();
+
+            
+            items.Add(new Item { Name = name, Price = price, Image = image });
+
+           
+            Session["ProductInfo"] = items;
+
+           
+            return RedirectToAction("Index");
+        }
+    }
+}
